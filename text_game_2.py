92684b8fdd9a49e8ead745c82503e50b369ac468
Kevin Hughes <ploddinginsight@gmail.com>
2020-07-03 04:08:58 +0800

    might be done translating game loop into new format, now to debug it

--- text_game_2.py
+++ text_game_2.py
@@ -207,7 +207,7 @@ def game_func(command_input):
                             console_output += pathways_dictionary[command_list[0]].look_text[player_location]
                         else:
                             # otherwise we just print the default description:
-                            console_output += object_dictionary[command_list[0]].look_text["default"])
+                            console_output += object_dictionary[command_list[0]].look_text["default"]
                     else:
                         # we're looking at a location
                         console_output += object_dictionary[command_list[0]].look_text["default"]
@@ -232,7 +232,8 @@ def game_func(command_input):
                         #tried to look at a location we don't have a direct pathway to
                         tot_fail()
                 else:
-                    console_output += "I didn't recognize the name of what you're trying to look at.")
+                    console_output += "I didn't recognize the name of what you're trying to look at."
+                    output_type = "unsuccessful"
 
             #
             #       PICK UP
@@ -242,15 +243,18 @@ def game_func(command_input):
                     if object_dictionary[command_list[0]].location != "INVENTORY":
                         if object_dictionary[command_list[0]].can_pick_up:
                             object_dictionary[command_list[0]].location = "INVENTORY"
+                            output_type = "success"
                         else:
-                            pass
+                            output_type = "unsuccessful"
                         # either way there should be something here:
                         console_output += object_dictionary[command_list[0]].pick_up_text["default"]
                     else:
                         #tried to pick up something in the inventory.
                         console_output += "You already have that."
+                        output_type = "unsuccessful"
                 else:
-                    console_output += "I don't recognize the name of what you're trying to obtain.")
+                    console_output += "I don't recognize the name of what you're trying to obtain."
+                    output_type = "unsuccessful"
 
             #
             #       DROP
@@ -263,8 +267,10 @@ def game_func(command_input):
                     else:
                         # tried to drop something not in inventory
                         console_output += "That object is not in your inventory so you can't put it down."
+                        output_type = "unsuccessful"
                 else:
-                    console_output += "I don't recognize the name of what you're trying to get rid of.")
+                    console_output += "I don't recognize the name of what you're trying to get rid of."
+                    output_type = "unsuccessful"
 
             #
             #       TRAVEL!
@@ -276,25 +282,28 @@ def game_func(command_input):
                        console_output += object_dictionary[command_list[0]].go_to_text["default"]
                     else:
                         # since location matches, you tried to go to where you are!
-                        console_output += "You are already there.")
+                        console_output += "You are already there."
+                        output_type = "unsuccessful"
                 elif find_location():  # next check outside the current location for other locations
                     for obj in pathways_dictionary:
                         if pathways_dictionary[obj].location == player_location and pathways_dictionary[obj].location2 == command_list[0] and pathways_dictionary[obj].pathway_open:
                             # found a way in so
                             # put player in new location
                             player_location = command_list[0]
+                            output_type = "success"
                             console_output += object_dictionary[command_list[0]].go_to_text["default"]
                             # switch locations on pathway objects with location2 = new location
                             for pathway in pathways_dictionary:
                                 if pathways_dictionary[pathway].location2 == player_location:
                                     pathways_dictionary[pathway].switch()
-                            break
+                            break #this break causes us to skip the for:else clause immediately following.
                     else:
                         # this is skipped if we found a way in through any pathway.
                         console_output += "I recognize the place you mentioned but you can't get there from here."
-
+                        output_type = "unsuccessful"
                 else:
-                    console_output += "I'm sorry, I don't recognize the name of where you're trying to go.")
+                    console_output += "I'm sorry, I don't recognize the name of where you're trying to go."
+                    output_type = "unsuccessful"
 
             #
             #       USE AN OBJECT!
@@ -312,15 +321,19 @@ def game_func(command_input):
                                     object2 = command_list[0]
                                     if object2 in object_dictionary[object1].use_text:
                                         console_output += object_dictionary[object1].use_text[object2]
+                                        output_type = "success"
                                     else:
                                         #no designated use text for the target
                                         console_output += object_dictionary[object1].use_text["default"]
                                 else:
                                     console_output += "I don't recognize the target of that action."
+                                    output_type = "unsuccessful"
                             else:
                                 console_output += "That object needs a target (a direct object) to be used.  Try using it 'on' or 'with' something else that is present."
+                                output_type = "unsuccessful"
                         else:
                             console_output += "You need to pick up that object to use it."
+                            output_type = "unsuccessful"
                     else:
                         # object doesn't need a target to be used, so we just print the use text
                         if object1 in pathways_dictionary:
@@ -330,91 +343,75 @@ def game_func(command_input):
                         else:
                             console_output += object_dictionary[object1].use_text["default"]
                 else:
-                    console_output += "I don't recognize what object you're trying to use.")
-
-
-
-
-
-
-
-
-
-
-            #LEFT OFF HERE
-
-
-
-
-
-
-
+                    console_output += "I don't recognize what object you're trying to use."
+                    output_type = "unsuccessful"
 
             #
             #   SPECIAL VERBS
             #
-            elif len(command_list) and command_list[0] in special_verbs:
+            elif command_list[0] in special_verbs:
                 object1 = special_verbs[command_list[0]]
                 del command_list[0]
                 if find_obj():
                     object2 = command_list[0]
-                command_list = ["USE", object1, "WITH", object2]
-                loop_again = True
+                    command_list = ["USE", object1, "WITH", object2]
+                    loop_again = True
                 else:
-                if object_dictionary[object1].location == "INVENTORY":
-                    console_output += "That command needs a target (a direct object).  Try again with one of the objects that is present.")
+                    #did not find target
+                    if object_dictionary[object1].location == "INVENTORY":
+                        console_output += "That command needs a target (a direct object).  Try again with one of the objects that is present."
+                        output_type = "unsuccessful"
+                    else:
+                        # Got rid of the below error message because it's potentially a clue to the player.
+                        #console_output += "You cannot use that command because you don't have the appropriate object in your inventory.")
+                        #instead:
+                        tot_fail()
 
-                else:
-                # I'm not totally happy with this error message because it could be used as a clue... Hmm...
-                console_output += "You cannot use that command because you don't have the appropriate object in your inventory.")
-
-                #
-                #   MISC
-                #
-                elif strip_off(["LOOK", "AROUND"]):
+            #
+            #   MISC
+            #
+            elif strip_off(["LOOK", "AROUND"]):
                 command_list = ["LOOK", "AT", "ROOM"]
                 loop_again = True
-                elif strip_off(["LOOK", "THROUGH"]) or strip_off(["PEER", "THROUGH"]):
-                if find_obj() and command_list[0] in pathways_dictionary:
-                    console_output += pathways_dictionary[command_list[0]].look_thru_text[player_location])
-                else:
-                console_output += "You can generally only look through pathways that lead to other locations (such as doorways.)")
 
-                elif strip_off(["GO", "THROUGH"]) or strip_off(["PASS", "THROUGH"]):
+            elif strip_off(["LOOK", "THROUGH"]) or strip_off(["PEER", "THROUGH"]):
+                if find_obj() and command_list[0] in pathways_dictionary:
+                    console_output += pathways_dictionary[command_list[0]].look_thru_text[player_location]
+                else:
+                    console_output += "You can generally only look through pathways that lead to other locations (such as doorways.)"
+                    output_type = "unsuccessful"
+
+            elif strip_off(["GO", "THROUGH"]) or strip_off(["PASS", "THROUGH"]):
                 if find_obj():
-                    if
-                command_list[0] in pathways_dictionary:
-                command_list = ["USE", command_list[0]]
-                loop_again = True
+                    if command_list[0] in pathways_dictionary:
+                        command_list = ["USE", command_list[0]]
+                        loop_again = True
+                    else:
+                        tot_fail()
                 else:
-                tot_fail()
-                else:
-                tot_fail()
-                elif strip_off(["HELP"]):
-                press("   Help   ")
-                command_counter -= 1  # cancel out timer increment
-                elif strip_off(["ABOUT"]):
-                press("   About   ")
-                command_counter -= 1  # cancel out timer increment
-                elif strip_off(["MUTE"]) or strip_off(["TURN", "SOUND", "OFF"]) or strip_off(["TURN", "SOUNDS", "OFF"]):
+                    tot_fail()
+
+            elif strip_off(["HELP"]):
+                console_output += help_text
+                command_counter -= 1
+            elif strip_off(["ABOUT"]):
+                console_output += about_text
+                command_counter -= 1
+            elif strip_off(["MUTE"]) or strip_off(["TURN", "SOUND", "OFF"]) or strip_off(["TURN", "SOUNDS", "OFF"]):
                 sound_toggle = False
                 console_output += "Game sounds have been turned off."
                 command_counter -= 1  # cancel out timer increment
-                elif strip_off(["UNMUTE"]) or strip_off(["TURN", "SOUND", "ON"]) or strip_off(["TURN", "SOUNDS", "ON"]):
+            elif strip_off(["UNMUTE"]) or strip_off(["TURN", "SOUND", "ON"]) or strip_off(["TURN", "SOUNDS", "ON"]):
                 sound_toggle = True
+                output_type = "success"
                 console_output += "Game sounds have been turned on."
                 command_counter -= 1  # cancel out timer increment
 
-                #
-                #   TOTAL FAIL
-                #
-                elif len(command_list):  # We check this so that an accidental enter key doesn't erase what's on the screen.
+            else:
                 tot_fail()
-                else:
-                # only executes if there's no text on the command line:
-                command_counter -= 1  # cancel out timer increment
 
-                if output_type == "unsuccessful":
+        #just exited while loop
+        if output_type == "unsuccessful":
             command_counter -= 1 #back out the increment since we didn't understand it.
 
         #play sound based on output type:
